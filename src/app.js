696e1ea045ce0ef559c29b01e25a0caf426eb5cd
Jekabs Hincenbergs <jekabshincenbergs@Jekabss-MacBook-Pro.local>
2022-07-19 20:42:56 +0300

    Basic endpoint for desription generation

--- src/app.js
+++ src/app.js
@@ -4,17 +4,46 @@ const { Configuration, OpenAIApi } = require('openai')
 const app = express()
 const port = 3030
 
+app.use(express.json());
+
 const configuration = new Configuration({
     organization: "org-3SRdJ2nJnnolqEoHq8z6jqHN",
     apiKey: settings.OPENAI_API_KEY,
 });
 
+
+function generatePrompt(title, keywords, description) {
+    return `Generate product descriptions for:
+    
+    Product Title: ${title}
+    Product Keywords: ${keywords.join()}
+    Product Description: ${description}
+    `
+}
+
 const openai = new OpenAIApi(configuration);
 
-app.get('/', async (req, res) => {
+app.get('/health', async (req, res) => {
+    res.send("OK")
+})
 
-    const response = await openai.listModels();
-    res.json(response.data)
+
+app.post('/descriptions', async (req, res) => {
+
+    console.log(req.body)
+
+    const { title, keywords, description } = req.body
+
+    const prompt = generatePrompt(title, keywords, description)
+
+    const completion = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt,
+        temperature: 0.6
+    })
+
+
+    res.json({ text: completion.data })
 })
 
 app.listen(port, () => {
