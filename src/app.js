abe7b60c3436defd4d319a3abf7e4e896570b786
Jekabs <jekabs.hincenbergs@gmail.com>
2022-07-20 20:11:06 +0300

    start Shopify auth integration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const settings = require('./settings')
 const { Configuration, OpenAIApi } = require('openai')
+const { Shopify } = require('@shopify/shopify-api')
 const app = express()
 const port = 8000
 
@@ -13,6 +14,19 @@ const configuration = new Configuration({
     apiKey: settings.OPENAI_API_KEY,
 });
 
+
+const shopify = Shopify.Context.initialize({
+    API_KEY: settings.SHOPIFY_API_KEY,
+    API_SECRET_KEY: settings.SHOPIFY_API_SECRET_KEY,
+    SCOPES: [settings.SHOPIFY_SCOPES],
+    HOST_NAME: settings.SHOPIFY_HOST.replace(/https?:\/\//, ""),
+    HOST_SCHEME: settings.SHOPIFY_HOST_SCHEME,
+    // IS_EMBEDDED_APP: { boolean },
+    // API_VERSION: Shopify.ApiVersion.{ version }
+});
+
+
+const ACTIVE_SHOPIFY_SHOPS = {}
 
 function generatePrompt(title, keywords, description) {
     return `Generate product descriptions for:
@@ -51,6 +65,40 @@ app.post('/descriptions', async (req, res) => {
 
     res.json({ choices })
 })
+
+
+app.get("/shopify/login", async (req, res) => {
+    const authRoute = await Shopify.Auth.beginAuth(
+        req,
+        res,
+        settings.SHOPIFY_SHOP,
+        "http://localhost:3000/callback",
+        false
+    )
+    return res.send(authRoute)
+})
+
+
+app.get("/shopify/auth/callback", async (req, res) => {
+    try {
+        const session = await Shopify.Auth.validateAuthCallback(
+            req,
+            res,
+            req.query
+        )
+        ACTIVE_SHOPIFY_SHOPS[SHOP] = session.scope
+        console.log(session.accessToken);
+    } catch (error) {
+        console.log(error)
+    }
+
+    console.log(req.query.shop)
+    console.log(req.query.host)
+
+    res.send("Auth?")
+
+})
+
 
 app.listen(port, () => {
     console.log(`APP HAS STARTED`)
--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,11 @@
 require('dotenv').config()
 
 module.exports = {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SHOPIFY_SHOP: process.env.SHOPIFY_SHOP,
+    SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
+    SHOPIFY_API_SECRET_KEY: process.env.SHOPIFY_API_SECRET_KEY,
+    SHOPIFY_SCOPES: process.env.SHOPIFY_SCOPES,
+    SHOPIFY_HOST: process.env.SHOPIFY_HOST,
+    SHOPIFY_HOST_SCHEME: process.env.SHOPIFY_HOST_SCHEME
 }
